ba558cb3bab4809211cb10a57a1a8747430a006b
Nazar Kudatsky <divipi1996@icloud.com>
2021-05-31 09:56:11 +0300

    Updates

---
+++ precache-manifest.58ee5da4d6f067bc2903238c1bab20a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a634758504dfdce355dd3ee5e6ccb10",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4b964fc25ecc0b658447",
+    "url": "./static/js/2.18166693.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.18166693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50a79c0c26a00088ae19",
+    "url": "./static/js/main.32c32816.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cb55ce10f2eb2a4d10df16c6247cd130.js"
+  "./precache-manifest.58ee5da4d6f067bc2903238c1bab20a3.js"
 );
 
 self.addEventListener('message', (event) => {
